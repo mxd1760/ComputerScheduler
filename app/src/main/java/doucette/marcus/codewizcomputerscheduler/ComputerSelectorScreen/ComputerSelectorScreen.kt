64838cddf6ec3bb508d2f6de77bcf574cb5ceed6
Marcus Doucette <Marz3587@gmail.com>
2025-02-19 16:53:02 -0500

    start

---
+++ app/src/main/java/doucette/marcus/codewizcomputerscheduler/ComputerSelectorScreen/ComputerSelectorScreen.kt
@@ -0,0 +1,14 @@
+package doucette.marcus.codewizcomputerscheduler.ComputerSelectorScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ComputerSelectorScreen(modifier: Modifier = Modifier) {
+    val vm = viewModel<ComputerSelectorScreenViewModel>()
+}
+
+@Composable
+fun DumbComputerSelectorScreen(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/doucette/marcus/codewizcomputerscheduler/ComputerSelectorScreen/ComputerSelectorScreenViewModel.kt
@@ -0,0 +1,8 @@
+package doucette.marcus.codewizcomputerscheduler.ComputerSelectorScreen
+
+import androidx.lifecycle.ViewModel
+
+class ComputerSelectorScreenViewModel: ViewModel() {
+
+
+}
